80b652a5badd09e1a1746dea20288587eea1ab35
Niv Aiges <71957436+nivaiges@users.noreply.github.com>
2026-01-14 22:47:25 -0500

    config js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Configuration for Google Sheets Backend
 // Replace this URL with your Google Apps Script Web App URL
-const GOOGLE_SCRIPT_URL = 'YOUR_SCRIPT_URL_HERE';
+const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwu5Rnk_7OhojcZzHvVeykwdo-FysRBnuPFR967ZjvgV3Fh4XU4u2Ax6MzJ2UU1eZub/exec';
 
 // Instructions:
 // 1. Follow SETUP_GUIDE.md to create your Google Sheet and deploy the script
